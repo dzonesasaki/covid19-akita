b1fbda817cc43c4aa8a10c5e972642040a1932a9
YuAoike <x31415926535x@gmail.com>
2020-03-31 09:58:21 +0900

    カード内の注意書ki

--- components/OpenDataLink.vue
+++ components/OpenDataLink.vue
@@ -1,6 +1,6 @@
 <template>
   <a class="OpenDataLink" :href="url" target="_blank" rel="noopener noreferrer">
-    {{ $t('オープンデータを入手') }}
+    {{ $t('県公式サイトで見る') }}
     <v-icon
       class="ExternalLinkIcon"
       size="15"

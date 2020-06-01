27d69f54d10fe512c59683cb6ef5f9e9b594a460
YuAoike <x31415926535x@gmail.com>
2020-06-01 17:37:50 +0900

    データの更新、検査実施件数のデータを県が新たに公開したものに変更

--- components/cards/InspectionPersonsNumberCard.vue
+++ components/cards/InspectionPersonsNumberCard.vue
@@ -1,29 +1,29 @@
 <template>
   <v-col cols="12" md="6" class="DataCard">
     <time-bar-chart
-      :title="$t('感染者と濃厚接触者の検査実施数')"
+      :title="$t('1週間ごとの検査実施件数')"
       :title-id="'number-of-inspection-persons'"
       :chart-id="'number-of-inspection-persons'"
       :chart-data="graphData"
       :date="data.date"
       :unit="$t('人')"
-      :url="'https://www.pref.akita.lg.jp/pages/archive/47957#houdouhappyou'"
+      :url="'https://www.pref.akita.lg.jp/pages/archive/47957#kennsa'"
       :source="$t('県公式サイトで見る')"
     >
       <template v-slot:description>
         <ul>
           <li>
-            {{ $t('（注）報道資料で公開されている検査のみを計上する') }}
+            {{ $t('（注）県、秋田市が実施した件数であり、民間機関が実施した件数は含まない') }}
           </li>
           <li>
             {{
-              $t('（注）チャーター機帰国者、クルーズ船乗客等は含まれていない')
+              $t('（注）退院時の陰性確認用の検査等の件数は含まない')
             }}
           </li>
           <li>
             {{
               $t(
-                '（注）速報値として公開するものであり、後日確定データとして修正される場合あり'
+                '（注）後日修正される場合あり'
               )
             }}
           </li>
